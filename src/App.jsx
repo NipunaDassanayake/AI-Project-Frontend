a56f6a74863940c1c48e09ebe9e8a047ce816ebf
NipunaDassanayake <nipuna920@gmail.com>
2024-07-31 15:17:17 +0530

    New

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Button } from "./components/ui/button"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+function App() {
+ 
+
+  return (
+    <main className="p-4">
+      <Button>Click Me</Button>
+
+      <Accordion type="single" collapsible>
+  <AccordionItem value="item-1">
+    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
+
+      
+    </main>
+  )
+}
+
+export default App
---
+++ src/Button/Button.jsx
@@ -0,0 +1,10 @@
+
+function Button() {
+ 
+
+  return (
+    <button className="border border-black px-4 py-2 rounded text-white bg-blue-500 ">Click Me</button>
+  )
+}
+
+export default Button;
